7ac68d63ad79b13cd99f223cc342d8d1150b88a8
Muhammad Haikal <75706383+justClizz@users.noreply.github.com>
2024-04-18 19:38:19 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -16,14 +16,13 @@ export default function Home() {
   </div>
   <div class="flex-none">
     <div class="dropdown dropdown-end">
-    <details class="dropdown">
-  <summary tabindex="0" class="m-1 btn">
-    <div class="indicator">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <div class="indicator">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
           <span class="badge badge-sm indicator-item">8</span>
         </div>
-    </summary>
-  <ul tabindex="0" class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+      </div>
+      <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
         <div class="card-body">
           <span class="font-bold text-lg">8 Items</span>
           <span class="text-info">Subtotal: $999</span>
@@ -32,8 +31,21 @@ export default function Home() {
           </div>
         </div>
       </div>
-  </ul>
-</details>
+    </div>
+        <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <div class="indicator">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+          <span class="badge badge-xs badge-primary indicator-item"></span>
+        </div>
+      </div>
+      <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+        <div class="card-body">
+          <span class="font-bold text-lg">This is nontification</span>
+          <span class="text-info">Your mother</span>
+        </div>
+      </div>
+    </div>
     <div class="dropdown dropdown-end">
       <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
@@ -44,7 +56,6 @@ export default function Home() {
         <li>
           <a class="justify-between">
             Profile
-            <span class="badge">New</span>
           </a>
         </li>
         <li><a>Settings</a></li>
