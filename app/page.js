e505020c38dc3c2edfa69f96faa848d05bb16cdb
Muhammad Haikal <75706383+justClizz@users.noreply.github.com>
2024-04-18 20:03:32 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,14 @@ import Navbar from "../app/navbarDefault"
 export default function NavbarDefault() {
   return (
      <>
+        <html lang="en">
+    <head>
+<title>risecloth. Web Store</title>
+    </head>
+      <body>    
     <Navbar />
+    </body>
+    </html>
     </>
     )
 }
