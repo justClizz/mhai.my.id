5a668f08ed5343d76c4024149886d34341831e78
Muhammad Haikal <75706383+justClizz@users.noreply.github.com>
2024-04-18 19:27:03 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -16,13 +16,14 @@ export default function Home() {
   </div>
   <div class="flex-none">
     <div class="dropdown dropdown-end">
-      <summary tabindex="0" role="button" class="btn btn-ghost btn-circle">
-        <div class="indicator">
+    <details class="dropdown">
+  <summary tabindex="0" class="m-1 btn">
+    <div class="indicator">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
           <span class="badge badge-sm indicator-item">8</span>
         </div>
-      </summary>
-      <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+    </summary>
+  <ul tabindex="0" class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
         <div class="card-body">
           <span class="font-bold text-lg">8 Items</span>
           <span class="text-info">Subtotal: $999</span>
@@ -31,7 +32,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </div>
+  </ul>
+</details>
     <div class="dropdown dropdown-end">
       <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
