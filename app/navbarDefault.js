cb31fed076cf2ab832785b04b18fdfab7638f5ac
Muhammad Haikal <75706383+justClizz@users.noreply.github.com>
2024-04-18 20:20:40 +0700

    Update navbarDefault.js

--- app/navbarDefault.js
+++ app/navbarDefault.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function NavbarDefault() {
   return (
     <>
-<div class="navbar bg-base-100">
+<div class="navbar bg-white-500">
   <div class="flex-1">
     <a class="btn btn-ghost text-xl">daisyUI</a>
   </div>
