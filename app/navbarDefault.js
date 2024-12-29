ac5aece35dee84bff171de7b06884cc2b8ab3b76
Muhammad Haikal <75706383+justClizz@users.noreply.github.com>
2024-12-29 19:53:03 +0700

    Update navbarDefault.js

--- app/navbarDefault.js
+++ app/navbarDefault.js
@@ -1,6 +1,8 @@
 import Script from 'next/script'
 import Link from "next/link";
- 
+import Image from 'next/image'
+import img from 'next/image'
+
 export default function NavbarDefault({ tambahan }) {
   return (
   <body data-theme="light">
