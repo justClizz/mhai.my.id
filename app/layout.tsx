4a097fbd9c1c3f7fe29afe5e72e15bd69a1dcfc4
Muhammad Haikal <75706383+justClizz@users.noreply.github.com>
2024-12-29 20:51:59 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import { Font } from 'next/font/google'
+import { font } from 'next/font/google'
  
 // If loading a variable font, you don't need to specify the font weight
 const fontA = Inter({
