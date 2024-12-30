ca715edc310427c28271b649dbb0d5628afd335f
Muhammad Haikal <king.haikalganteng@gmail.com>
2024-12-30 19:02:13 +0700

    Refactor NavbarDefault component to improve layout and navigation links

--- app/navbarDefault.js
+++ app/navbarDefault.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function NavbarDefault({ tambahan }) {
   return (
     <>
-    <div class="navbar backdrop-blur-sm bg-black/90 fixed bg-base-100" 
+    <div class="navbar backdrop-blur-sm bg-black/90 sticky bg-base-100" 
 style={{
     borderTopLeftRadius: 13,
     borderTopRightRadius: 13,
@@ -13,57 +13,27 @@ style={{
     boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"
 }}>
       <div class="flex-1">
+        <br />
         <Link href="/" class="text-xl text-white">Muhammad Haikal Ali Abdillah</Link>
       </div>
+      
       <div class="flex-none">
-        <div class="dropdown dropdown-end">
-          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-            <div class="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-              <span class="badge badge-sm indicator-item">8</span>
-            </div>
-          </div>
-          <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-            <div class="card-body">
-              <span class="font-bold text-lg">8 Items</span>
-              <span class="text-info">Subtotal: $999</span>
-              <div class="card-actions">
-                <Link href="/cart" class="btn btn-primary btn-block">View cart</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-            <div class="dropdown dropdown-end">
-          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-            <div class="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-              <span class="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </div>
-          <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-            <div class="card-body">
-              <span class="font-bold text-lg">This is nontification</span>
-              <span class="text-info">Your mother</span>
-            </div>
-          </div>
-        </div>
-        <div class="dropdown dropdown-end">
-          <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-            <div class="w-10 rounded-full">
-              <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            </div>
-          </div>
-          <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li>
-              <a class="justify-between">
-                Profile
-              </a>
-            </li>
-            <li><a>Settings</a></li>
-            <li><a>Logout</a></li>
+    <ul class="menu menu-horizontal px-1">
+      <li><a><Link href="/">Homepage</Link></a></li>
+      <li><a><Link href="#about">About Me</Link></a></li>
+      <li><a><Link href="#project">Project</Link></a></li>
+      <li><button class="btn btn-info"><Link href="#contact">Contact Me!</Link></button></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul class="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
           </ul>
-        </div>
-      </div>
+        </details>
+      </li>
+    </ul>
+  </div>
     </div>
             </>
 
