5e4861d564287cc5639107cc197f8a5530f00033
Muhammad Haikal <king.haikalganteng@gmail.com>
2024-12-31 20:31:33 +0700

    eeeeeeeee

--- app/navbarDefault.js
+++ app/navbarDefault.js
@@ -14,7 +14,7 @@ export default function NavbarDefault({ tambahan }) {
       </div>
       
       <div class="flex-none">
-    <ul class="menu menu-horizontal px-1">
+    <ul class="menu menu-horizontal px-1 inline md:none">
       <li><button class="btn btn-link text-white"><Link href="/">Homepage</Link></button></li>
       <li><button class="btn btn-link text-white"><Link href="#about">About Me</Link></button></li>
       <li><button class="btn btn-link text-white"><Link href="#project">Project</Link></button></li>
